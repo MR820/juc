35e05682def4bdd44260370734521e5d43d9130a
邢志伟 <xingzhiwei8924@yundasys.com>
2021-04-12 10:11:39 +0800

    CyclicBarrier

--- src/main/java/cn/org/wyxxt/juc/c_020_ReentrantLock/T07_TestCyclicBarrier.java
+++ src/main/java/cn/org/wyxxt/juc/c_020_ReentrantLock/T07_TestCyclicBarrier.java
@@ -7,28 +7,30 @@ public class T07_TestCyclicBarrier {
     public static void main(String[] args) {
         //CyclicBarrier barrier = new CyclicBarrier(20);
 
-        CyclicBarrier barrier = new CyclicBarrier(20, () -> System.out.println("����"));
+        CyclicBarrier barrier = new CyclicBarrier(20, () -> System.out.println("*"));
 
         /*CyclicBarrier barrier = new CyclicBarrier(20, new Runnable() {
             @Override
             public void run() {
-                System.out.println("���ˣ�����");
+                System.out.println("*");
             }
         });*/
 
         for(int i=0; i<100; i++) {
 
-                new Thread(()->{
-                    try {
-                        barrier.await();
+      new Thread(
+              () -> {
+                try {
+                    System.out.println("thread");
+                    barrier.await();
 
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } catch (BrokenBarrierException e) {
-                        e.printStackTrace();
-                    }
-                }).start();
-            
+                } catch (InterruptedException e) {
+                  e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                  e.printStackTrace();
+                }
+              })
+          .start();
         }
     }
 }
